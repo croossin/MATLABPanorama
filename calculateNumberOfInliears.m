7768a0b7e3f0aa233836f18f16f2e0fbbf4d435c
Chase Roossin <croossin94@gmail.com>
2015-10-26 17:00:37 -0500

    Switched p1 and p2 when calculating number of outliers

--- calculateNumberOfInliears.m
+++ calculateNumberOfInliears.m
@@ -6,9 +6,9 @@ inlierCount = 0;
 
 %Go through all points determing if they are an inlier or not
 for i=1: n
-   p1Homogenius = [p1(i,:),1];
-   q = homography*(p1Homogenius');
-   distance = getEuclideanDistance(q(1:2)', p2(i,:));
+   p2Homogenius = [p2(i,:),1];
+   q = homography*(p2Homogenius');
+   distance = getEuclideanDistance(q(1:2)', p1(i,:));
    disp(distance);
    if distance < 3
       inlierCount = inlierCount + 1; 
