74b13375e087b585e6296b632165cce9ec25c277
Chase Roossin <croossin94@gmail.com>
2015-10-26 17:26:19 -0500

    Believe to have finished section 2 and 3

--- calcH.m
+++ calcH.m
@@ -62,15 +62,14 @@ end
 arrayOfInlierRowValues = getInlierRowValues(bestHomography, p1, p2, n);
 
 %Use only inlier points
-
-A = zeros(n*3,9);
-b = zeros(n*3,1);
-for i=1:n
-    A(3*(i-1)+1,1:3) = [p2(i,:),1];
-    A(3*(i-1)+2,4:6) = [p2(i,:),1];
-    A(3*(i-1)+3,7:9) = [p2(i,:),1];
-    b(3*(i-1)+1:3*(i-1)+3) = [p1(i,:),1];
+for i=1: length(arrayOfInlierRowValues)
+     rowValue = arrayOfInlierRowValues(i);
+     A(3*(x-1)+1,1:3) = [p2(rowValue,:),1];
+     A(3*(x-1)+2,4:6) = [p2(rowValue,:),1];
+     A(3*(x-1)+3,7:9) = [p2(rowValue,:),1];
+     b(3*(x-1)+1:3*(x-1)+3) = [p1(rowValue,:),1];
 end
+
 x = (A\b)';
 H = [x(1:3); x(4:6); x(7:9)];
 
