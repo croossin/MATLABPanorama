a617adc00e49c360789c412e8adb3431b7dcc315
Chase Roossin <croossin94@gmail.com>
2015-10-26 20:21:42 -0500

    Name change AGAIN

--- calculateNumberOfInliers.m
+++ calculateNumberOfInliers.m
@@ -6,8 +6,8 @@ inlierCount = 0;
 
 %Go through all points determing if they are an inlier or not
 for i=1: n
-   p2Homogenius = [p2(i,:),1];
-   q = homography*(p2Homogenius');
+   p2homogeneous = [p2(i,:),1];
+   q = homography*(p2homogeneous');
    distance = getEuclideanDistance(q(1:2)', p1(i,:));
    %Inlier - increase count
    if distance < 3
