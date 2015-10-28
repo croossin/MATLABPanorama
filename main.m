48ca77842aa3a8632674bd9ebef67994907dd065
Chase Roossin <croossin94@gmail.com>
2015-10-27 20:46:49 -0500

    Fixed outer border to not be 0's

--- main.m
+++ main.m
@@ -195,15 +195,21 @@ for i = 1 : length(warped_images)
     tempImage(height,:,:) = 1;
     tempImage(:,1,:) = 1;
     tempImage(:,width,:) = 1;
+    %Make weight
     tempImage = bwdist(tempImage, 'euclidean');
+    tempImage(1,:,:) = 1;
+    tempImage(height,:,:) = 1;
+    tempImage(:,1,:) = 1;
+    tempImage(:,width,:) = 1;
     centerPtValue = tempImage(uint8((height/2)), uint8((width/2)));
     weightImage = zeros(height,width,depth);
+    %Adjust weight (0,1]
     for x=1:height
        for y=1:width
            weightImage(x,y,:) = tempImage(x,y,:)/centerPtValue;
        end
     end
-    weightImage
+    
     panorama_image = panorama_image + warped_images{i};
 end
 
